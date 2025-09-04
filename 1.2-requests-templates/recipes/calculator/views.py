52bda258efc027f4adef2175ca3ba30f8a8b0b24
Neto test <liza.pryaxina@mail.ru>
2025-09-04 21:23:48 +0300

    fdа

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,9 +16,50 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
+    'meatballs': {
+        'фарш, г': 100,
+        'рис, г': 20,
+        'мука, ст': 0.3,
+        'соль, ч.л.': 0.5
+    }
     # можете добавить свои рецепты ;)
 }
 
+def calculate_recipe_view(request, recipe_name):
+
+    if recipe_name in DATA:
+        data = DATA[recipe_name]
+        servings = request.GET.get('servings', None)
+
+        if servings:
+            result = dict()
+            for item, value in data.items():
+                new_value = value * int(servings)
+                result[item] = new_value
+            context = {
+                'recipe_name': recipe_name,
+                'recipe': result
+            }
+        else:
+            context = {
+                'recipe': {
+                    'ing1': 2,
+                    'ing2': 4,
+                }
+            }
+
+    else:
+        context = None
+
+    return render(request, template_name='calculator/index.html', context=context)
+
+
+def home_view(request):
+
+    all_recipes = list(DATA.keys())
+    context = {'all_recipes': all_recipes}
+
+    return render(request, template_name='home/home.html', context=context)
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
 
+from calculator.templates.views import calculate_recipe_view, home_view
+
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('recipes/', home_view, name='home'),
+    path('recipes/<recipe_name>/', calculate_recipe_view, name='calculate_recipe')
     # здесь зарегистрируйте вашу view-функцию
 ]
